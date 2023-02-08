46633bbb5b614575913939d6549054d65a796f13
Camper <chaudharyishwar2004@gmail.com>
2023-02-08 16:34:35 +0530

    My first checking

---
+++ springboot-backend/src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,91 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email_id")
+	private String emailId;
+	
+	@Column(name="country")
+    private String country;
+	
+	@Column(name="gender")
+    private String gender;
+	
+	@Column(name="dob")
+    private String dob;
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(String firstName, String lastName, String emailId, String country, String gender, String dob) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+		this.country = country;
+		this.gender = gender;
+		this.dob = dob;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getcountry() {
+		return country;
+	}
+	public void setcountry(String country) {
+		this.country = country;
+	}
+	public String getgender() {
+		return gender;
+	}
+	public void setgender(String gender) {
+		this.gender = gender;
+	}
+	public String getdob() {
+		return dob;
+	}
+	public void setdob(String dob) {
+		this.dob = dob;
+	}
+}
